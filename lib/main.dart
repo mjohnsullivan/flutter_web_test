8b4b62f22911513c1b6539282b5dd28bdac12ea2
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-12-06 14:09:00 -0800

    const'd up the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-const textFiller = '''
-Lorem ipsum dolor sit amet, vix case modus impedit ex, te congue munere quo. Utamur aliquip signiferumque et vim. Solum legimus moderatius eos no, mel cu quando perfecto aliquando. Epicuri adipiscing eum te, cu dicit doming similique cum, suas summo pertinax has et. Vix fugit legere doctus no.
-''';
+const textFiller =
+    'Lorem ipsum dolor sit amet, vix case modus impedit ex, te congue munere quo.' +
+        'Utamur aliquip signiferumque et vim. Solum legimus moderatius eos no, mel cu' +
+        'quando perfecto aliquando. Epicuri adipiscing eum te, cu dicit doming similique' +
+        'cum, suas summo pertinax has et. Vix fugit legere doctus no.';
 
 void main() => runApp(MyApp());
 
@@ -20,12 +22,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text and Icon Test'),
+        leading: const Icon(Icons.question_answer),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text(textFiller),
+            const Text(textFiller),
             Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: const [
@@ -33,7 +39,7 @@ class HomePage extends StatelessWidget {
                   Icon(Icons.access_time),
                   Icon(Icons.add_a_photo),
                 ]),
-            Text(textFiller),
+            const Text(textFiller),
             Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: const [
@@ -41,7 +47,7 @@ class HomePage extends StatelessWidget {
                   Icon(Icons.access_time),
                   Icon(Icons.add_a_photo),
                 ]),
-            Text(textFiller),
+            const Text(textFiller),
           ],
         ),
       ),
